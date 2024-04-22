287767a3b8a266b4f6431f76f546b7cde435cd79
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-04-22 19:15:21 +0300

    feat: set search params

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,12 +1,36 @@
 "use client";
 
 import { IDropdownOption } from "@/types/general";
-import { useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useState } from "react";
 import Dropdown from "../Dropdown";
 import { IconSearch } from "../Icons";
 
 const Filter = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
   const [region, setRegion] = useState<string>();
+  const [searchName, setSearchName] = useState<string>();
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams("");
+      params.set(name, value);
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  const deleteQueryString = useCallback(
+    (name: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.delete(name);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
   return (
     <div className=" flex flex-col md:flex-row">
       <div className="relative">
@@ -14,6 +38,9 @@ const Filter = () => {
         <input
           className="w-full rounded py-4 text-sm pl-20 dark:bg-dark-secondary md:w-[340px]"
           placeholder="Search for a country..."
+          onChange={(e: React.FormEvent<HTMLInputElement>) => {
+            setSearchName(e.currentTarget.value);
+          }}
         />
       </div>
       <Dropdown
@@ -51,7 +78,31 @@ const Filter = () => {
         <button className="rounded-lg border border-dark-secondary px-4 py-3 text-sm">
           Clear
         </button>
-        <button className="rounded-lg box-border bg-white dark:bg-dark-secondary px-4 py-3 text-sm">
+        <button
+          onClick={() => {
+            router.push(
+              `${pathname}?${
+                region ? createQueryString("region", region) : ""
+              }${searchName ? "&" + createQueryString("name", searchName) : ""}`
+            );
+
+            // if (region) {
+            //   router.push(pathname + "?" + createQueryString("region", region));
+            // } else {
+            //   console.log("else");
+            //   router.push(pathname + "?" + deleteQueryString("region"));
+            // }
+            // if (searchName) {
+            //   router.push(
+            //     pathname + "?" + createQueryString("name", searchName)
+            //   );
+            // } else {
+            //   console.log("else");
+            //   router.push(pathname + "?" + deleteQueryString("name"));
+            // }
+          }}
+          className="rounded-lg box-border bg-white dark:bg-dark-secondary px-4 py-3 text-sm"
+        >
           Search
         </button>
       </div>
