876963d820947f356c0609849bed363384c42b65
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-04-23 11:09:39 +0300

    feat: get all contries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 import Filter from "@/components/Filter";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log("data :", data);
   return (
     <>
       <Filter />
