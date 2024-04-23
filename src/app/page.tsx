f6191a0224185e36bf5d87defb405bc599c51125
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-04-23 11:19:19 +0300

    feat: filter by region

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import Filter from "@/components/Filter";
 
-async function getData() {
+async function getData(region?: string) {
+  if (region) {
+    const res = await fetch(`https://restcountries.com/v3.1/region/${region}`);
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return res.json();
+  }
   const res = await fetch("https://restcountries.com/v3.1/all");
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -8,8 +15,12 @@ async function getData() {
   return res.json();
 }
 
-export default async function Home() {
-  const data = await getData();
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  const data = await getData(searchParams?.region as string);
   console.log("data :", data);
   return (
     <>
