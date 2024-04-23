04f30ae44c9b9808a529eb5b83502c5f84f7d949
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-04-23 11:44:16 +0300

    feat: fetch data by name and region

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import Filter from "@/components/Filter";
 
-async function getData(region?: string) {
+async function getData(region?: string, name?: string) {
   if (region) {
     const res = await fetch(`https://restcountries.com/v3.1/region/${region}`);
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return res.json();
+  }
+  if (name) {
+    const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
     if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
@@ -20,7 +27,10 @@ export default async function Home({
 }: {
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  const data = await getData(searchParams?.region as string);
+  const data = await getData(
+    searchParams?.region as string,
+    searchParams?.name as string
+  );
   console.log("data :", data);
   return (
     <>
